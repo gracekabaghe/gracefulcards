d8d00c43026fe1af45a8aef7c71846c5b57e8c5f
gracekabaghe <gracema2002@gmail.com>
2014-04-23 20:52:24 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#gracefulcards
+
+A website for cards
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gracefulcards::Application.config.secret_token = 'f0aca13c59155af2d9620267f8e45b40c6c6b200792f0215a931285da37ba43aad7cbbc1e059597364a9a9d87c4191f7097eaaab7337bdc4ab13b4638c82e39a'
