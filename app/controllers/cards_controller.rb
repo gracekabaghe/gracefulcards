f53732759001da731ec246e0ae837b14aa5d7af9
gracekabaghe <gracema2002@gmail.com>
2014-05-07 20:46:26 +0800

    added about page

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,6 @@
 class CardsController < ApplicationController
   def index
+    
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Gracefulcards::Application.routes.draw do
+  devise_for :users
+
   resources :cards
+
+  match "about" => 'welcome#about', via: :get
 
   root to: 'cards#index'  
 end
