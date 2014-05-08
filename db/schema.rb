833c7a55d00b99b31f67f0241e4cf0030a9a518e
gracekabaghe <gracema2002@gmail.com>
2014-05-08 17:03:06 +0800

    image uploader

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140505130900) do
+ActiveRecord::Schema.define(:version => 20140508072413) do
 
   create_table "cards", :force => true do |t|
     t.string   "category"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(:version => 20140505130900) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
